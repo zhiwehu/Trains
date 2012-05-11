02c17759b2317a55858c127461bb9e0f93557258
Jeffrey Hu <golettoo@gmail.com>
2012-05-12 00:06:44 +0800

    re-construct

--- tests.py
+++ tests.py
@@ -1,7 +1,7 @@
 __author__ = 'jeffrey'
 
 import unittest
-from trains import Assistant, City, Route
+from trains import CalMaxDistanceAssistant, CalMaxStopsAssistant, CalShortestAssistant, City, Route
 
 class TestAssistant(unittest.TestCase):
     def setUp(self):
@@ -24,47 +24,54 @@ class TestAssistant(unittest.TestCase):
         Route(a, e, 7)
 
         # Hire an assistant
-        self.assistant = Assistant()
-
+        self.max_dis_assistant = CalMaxDistanceAssistant()
         # The assistant need to study cities
-        self.assistant.add_cities([a, b, c, d, e])
+        self.max_dis_assistant.add_cities([a, b, c, d, e])
+
+        # Hire an assistant
+        self.max_stops_assistant = CalMaxStopsAssistant()
+        # The assistant need to study cities
+        self.max_stops_assistant.add_cities([a, b, c, d, e])
+
+        # Hire an assistant
+        self.shortest_route_assistant = CalShortestAssistant()
+        # The assistant need to study cities
+        self.shortest_route_assistant.add_cities([a, b, c, d, e])
 
     def test_get_distance(self):
-        self.assertEqual(9, self.assistant.get_distance("A-B-C"))
-        self.assertEqual(5, self.assistant.get_distance("A-D"))
-        self.assertEqual(13, self.assistant.get_distance("A-D-C"))
-        self.assertEqual(22, self.assistant.get_distance("A-E-B-C-D"))
-        self.assertEqual("NO SUCH ROUTE", self.assistant.get_distance("A-E-D"))
+        self.assertEqual(9, self.max_dis_assistant.get_distance("ABC"))
+        self.assertEqual(5, self.max_dis_assistant.get_distance("AD"))
+        self.assertEqual(13, self.max_dis_assistant.get_distance("ADC"))
+        self.assertEqual(22, self.max_dis_assistant.get_distance("AEBCD"))
+        self.assertEqual("NO SUCH ROUTE", self.max_dis_assistant.get_distance("AED"))
 
     def test_cal_max_stops_routes(self):
-        self.assistant.routes = []
-        self.assertEqual(0, len(self.assistant.routes))
-        self.assistant.cal_max_stops_routes("C", "C", 3)
-        self.assertEqual(2, len(self.assistant.routes))
+        self.max_stops_assistant.routes = []
+        self.assertEqual(0, len(self.max_stops_assistant.routes))
+        self.max_stops_assistant.cal_routes("C", "C", 3)
+        self.assertEqual(2, len(self.max_stops_assistant.routes))
 
     def test_print_exact_stops_routes(self):
-        self.assertEqual(3, len(self.assistant.get_exact_stops_routes("A", "C", 4)))
+        self.assertEqual(3, len(self.max_dis_assistant.get_exact_stops_routes("A", "C", 4)))
 
     def test_cal_shortest_route(self):
         # The shortest route of city A to city C
-        self.assistant.reset_shortest_route()
-        self.assertEqual(-1, self.assistant.shortest_distance)
-        self.assertEqual("", self.assistant.shortest_route)
-        self.assistant.cal_shortest_route("A", "C")
-        self.assertEqual(9, self.assistant.shortest_distance)
+        self.shortest_route_assistant.routes = []
+        self.assertEqual(0, len(self.shortest_route_assistant.routes))
+        self.shortest_route_assistant.cal_routes("A", "C")
+        self.assertEqual(9, self.shortest_route_assistant.get_distance(self.shortest_route_assistant.routes[0]))
 
-        # The shortest route of city A to city C
-        self.assistant.reset_shortest_route()
-        self.assertEqual(-1, self.assistant.shortest_distance)
-        self.assertEqual("", self.assistant.shortest_route)
-        self.assistant.cal_shortest_route("B", "B")
-        self.assertEqual(9, self.assistant.shortest_distance)
+        # The shortest route of city B to city B
+        self.shortest_route_assistant.routes = []
+        self.assertEqual(0, len(self.shortest_route_assistant.routes))
+        self.shortest_route_assistant.cal_routes("B", "B")
+        self.assertEqual(9, self.shortest_route_assistant.get_distance(self.shortest_route_assistant.routes[0]))
 
     def test_cal_all_routes(self):
-        self.assistant.routes = []
-        self.assertEqual(0, len(self.assistant.routes))
-        self.assistant.cal_all_routes("C", "C")
-        self.assertEqual(7, len(self.assistant.routes))
+        self.max_dis_assistant.routes = []
+        self.assertEqual(0, len(self.max_dis_assistant.routes))
+        self.max_dis_assistant.cal_routes("C", "C", 30)
+        self.assertEqual(7, len(self.max_dis_assistant.routes))
 
 if __name__ == "__main__":
     unittest.main()
--- trains.py
+++ trains.py
@@ -20,7 +20,6 @@ class City:
         else:
             raise Exception("NO SUCH ROUTE")
 
-
 class Route:
     def __init__(self, start_city, end_city, distance):
         self.start_city = start_city
@@ -31,7 +30,6 @@ class Route:
 class Assistant:
     def __init__(self):
         self.cities = {}
-        self.reset_shortest_route()
         self.routes = []
 
     def add_city(self, city):
@@ -44,20 +42,16 @@ class Assistant:
     def get_city(self, city_name):
         return self.cities[city_name]
 
-    def reset_shortest_route(self):
-        self.shortest_distance = -1
-        self.shortest_route = ""
-
     def get_distance(self, route):
         """
         Get distance of the route.
 
-        @param route: route string which split by '-', such as 'A-B-C'
+        @param route: route string such as 'ABC'
         """
         total_distance = 0
         try:
             if route:
-                city_list = route.split("-")
+                city_list = list(route)
                 start_city = self.get_city(city_list.pop(0))
                 while city_list:
                     end_city = self.get_city(city_list.pop(0))
@@ -68,24 +62,6 @@ class Assistant:
             return e.message
         return total_distance
 
-    def cal_max_stops_routes(self, start_city_name, end_city_name, max_stops):
-        """
-        Calculate how many routes can from first city of route_path to end_city.
-        The routes will be saved to self.routes list
-
-        @param start_city_name: the route path string, such as 'ABC'
-        @param end_city_name: the end city name string
-        """
-        if (len(start_city_name) - 1) > max_stops:
-            return
-
-        if len(start_city_name) > 1 and start_city_name.endswith(end_city_name):
-            self.routes.append(start_city_name)
-
-        end_city = self.get_city(start_city_name[-1])
-        for route in end_city.start_routes:
-            self.cal_max_stops_routes(start_city_name + route.end_city.city_name, end_city_name, max_stops)
-
     def get_exact_stops_routes(self, start_city_name, end_city_name, extra_stops):
         """
         Return the route list from start_city to end_city with extra stops.
@@ -94,7 +70,7 @@ class Assistant:
         @param end_city_name: end city name
         @param extra_stops: extra stops
         """
-        last_route_paths = [start_city_name,]
+        last_route_paths = [start_city_name, ]
         for stop in range(extra_stops):
             route_paths = []
             for route_path in last_route_paths:
@@ -102,7 +78,7 @@ class Assistant:
                 for route in city.start_routes:
                     new_route_path = route_path + route.end_city.city_name
                     route_paths.append(new_route_path)
-            # Please care about this need to copy a list, can not assignment
+                # Please care about this need to copy|clone a list, can not assignment
             last_route_paths = route_paths[:]
 
         routes = []
@@ -111,43 +87,65 @@ class Assistant:
                 routes.append(route)
         return routes
 
-    def cal_shortest_route(self, start_city_name, end_city_name, distance=0):
-        """
-        Calculate the shortest route from start city to end city.
-        The result will be save into self.shortest_route and self.shortest_distance
+    def check_value(self, route, value):
+        return False
 
-        @param start_city_name: start city name
-        @param end_city_name: end city name
-        """
-        if start_city_name.endswith(end_city_name) and distance > 0 and (
-        distance < self.shortest_distance or self.shortest_distance == -1):
-            self.shortest_route = start_city_name
-            self.shortest_distance = distance
+    def handle_route(self, route):
+        self.routes.append(route)
 
-        end_city = self.get_city(start_city_name[-1])
-        for route in end_city.start_routes:
-            if route.end_city.city_name in start_city_name[1:]:
-                # Check if the end city name has been in start_city_name, means repeat route, will not calculate.
-                continue
-            self.cal_shortest_route(start_city_name + route.end_city.city_name, end_city_name, distance + route.distance)
-
-    def cal_all_routes(self, start_city_name, end_city_name, distance=0, max_distance=30):
+    def cal_routes(self, start_city_name, end_city_name, value=0):
         """
-        Calculate all routes from start city to end city which max distance less then 30.
+        Calculate all routes from start city to end city which max value
+        The logic to check the max_value is in sub class
         The result save to self.routes.
 
         @param start_city_name: start city name
         @param end_city_name: end city name
         """
-        if distance >= max_distance:
+        if self.check_value(start_city_name, value):
             return
 
-        if distance > 0 and start_city_name.endswith(end_city_name):
-            self.routes.append("%s : %d" % (start_city_name, distance))
+        if len(start_city_name) > 1 and start_city_name.endswith(end_city_name):
+            self.handle_route(start_city_name)
 
         end_city = self.get_city(start_city_name[-1])
         for route in end_city.start_routes:
-            self.cal_all_routes(start_city_name + route.end_city.city_name, end_city_name, distance + route.distance)
+            self.cal_routes(start_city_name + route.end_city.city_name, end_city_name, value)
+
+class CalMaxDistanceAssistant(Assistant):
+    def __init__(self):
+        Assistant.__init__(self)
+
+    def check_value(self, route, value):
+        """
+        Check if the distance of route less than max distance of value
+        """
+        return self.get_distance(route) >= value
+
+class CalMaxStopsAssistant(Assistant):
+    def __init__(self):
+        Assistant.__init__(self)
+
+    def check_value(self, route, value):
+        """
+        Check if the route has more than max stop of value
+        """
+        return (len(route) - 1) > value
+
+class CalShortestAssistant(Assistant):
+    def __init__(self):
+        Assistant.__init__(self)
+
+    def check_value(self, route, value):
+        return route[1:].count(route[-1]) > 1
+
+    def handle_route(self, route):
+        if self.routes:
+            old_route = self.routes[0]
+            if self.get_distance(route) < self.get_distance(old_route):
+                self.routes[0] = route
+        else:
+            self.routes.append(route)
 
 if __name__ == "__main__":
     # Init cities
@@ -168,50 +166,57 @@ if __name__ == "__main__":
     Route(e, b, 3)
     Route(a, e, 7)
 
-    # Hire an assistant
-    assistant = Assistant()
-
+    # Hire another assistant for calculate max stops
+    assistant = CalMaxStopsAssistant()
     # The assistant need to study cities
     assistant.add_cities([a, b, c, d, e])
 
     # Please tell me the distance of route A-B-C
-    print assistant.get_distance("A-B-C")
+    print assistant.get_distance("ABC")
 
     # How much distance of route A-D ?
-    print assistant.get_distance("A-D")
+    print assistant.get_distance("AD")
 
     # And how much of A-D-C
-    print assistant.get_distance("A-D-C")
+    print assistant.get_distance("ADC")
 
     # And A-E-B-C-D ?
-    print assistant.get_distance("A-E-B-C-D")
+    print assistant.get_distance("AEBCD")
 
     # A-E-D ?
-    print assistant.get_distance("A-E-D")
+    print assistant.get_distance("AED")
 
     # The # of trips between city C and C with a max stops 3
     assistant.routes = []
-    assistant.cal_max_stops_routes("C", "C", 3)
-    print assistant.routes
+    assistant.cal_routes("C", "C", 3)
+    #print assistant.routes
     print len(assistant.routes)
 
     # Print how many routes between city A to C and extra stops if 4
     routes = assistant.get_exact_stops_routes("A", "C", 4)
-    print routes
+    #print routes
     print len(routes)
 
     # The shortest route of city A to city C
-    assistant.reset_shortest_route()
-    assistant.cal_shortest_route("A", "C")
-    print assistant.shortest_distance
-
+    # Hire another assistant for calculate shortest distance
+    assistant = CalShortestAssistant()
+    # The assistant need to study cities
+    assistant.add_cities([a, b, c, d, e])
+    assistant.routes = []
+    assistant.cal_routes("A", "C")
+    print assistant.get_distance(assistant.routes[0])
+    #print assistant.shortest_routeZ
     # The shortest route of city B to city B
-    assistant.reset_shortest_route()
-    assistant.cal_shortest_route("B", "B")
-    print assistant.shortest_distance
+    assistant.routes = []
+    assistant.cal_routes("B", "B")
+    print assistant.get_distance(assistant.routes[0])
 
     # The # of different routes from city C to C with distance less then 30
+    # Hire an assistant for calculate max distance
+    assistant = CalMaxDistanceAssistant()
+    # The assistant need to study cities
+    assistant.add_cities([a, b, c, d, e])
     assistant.routes = []
-    assistant.cal_all_routes("C", "C")
-    print assistant.routes
+    assistant.cal_routes("C", "C", 30)
+    #print assistant.routes
     print len(assistant.routes)
